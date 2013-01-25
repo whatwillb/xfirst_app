61542aaf762cbc0258340ecbeb0167794c9b8821
Bill Bonnefil <will@bonnefil.com>
2013-01-24 19:58:50 -0800

    Created first time app and all the initial files modified the ignore file to add a few more file types

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FirstApp::Application.config.secret_token = 'b25c22a310f51e0f57c5add60db206d061dbf0205ed7aacb914ad11b5b5671f486589c38130c7ae07792a39df673b320cd72fa35803ccb4231158eb931b9474f'
